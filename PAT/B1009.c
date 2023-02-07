b01529f807eaaee034b4d0f994c4c954b63768ec
志远 <50417000+AdorableLake@users.noreply.github.com>
2023-02-07 12:19:07 +0800

    Create B1009.c

---
+++ PAT/B1009.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+    char str[90];
+    gets(str);
+    int len=strlen(str), r=0, h=0;
+    char ans[90][90];
+    for(int i=0; i<len; i++) {
+        if(str[i] != ' ') ans[r][h++] = str[i];
+        else {
+            ans[r][h] = '\0';
+            r++;
+            h=0;
+        }
+    }
+    for(int i=r; i>=0; i--) {
+        printf("%s", ans[i]);
+        if(i>0) printf(" ");
+    }
+    /*
+    int num = 0;
+    char ans[90][90];
+    while(scanf("%s",ans[num])!=EOF) {
+        num++;
+    }
+    for (int i=num-1; i>=0; i++) {
+        printf("%s",ans[i]);
+        if(i>0) printf(" ");
+    }
+    */
+    return 0;
+}
